067a7cd5ee338b8d32b09231c82ed48114400feb
elisagc <elisagcubero@gmail.com>
2020-07-10 11:22:00 +0200

    some styles

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,18 +1,24 @@
 <template>
-  <nav class="navbar navbar-dark bg-dark">
-    <h1 class="navbar-brand">My Tasks</h1>
-    <md-button style="outline:none" v-on:click="setIsOpen()">
-      <md-icon style="color:white">add_circle_outline</md-icon>
-    </md-button>
-  </nav>
+    <nav class="navbar navbar-dark bg-dark">
+        <h1 class="navbar-brand">My Tasks</h1>
+        <md-button style="outline:none" v-on:click="setIsOpen()">
+            <md-icon style="color:white">add_circle_outline</md-icon>
+        </md-button>
+    </nav>
 </template>
+
+<style>
+.navbar-brand {
+    margin: 0;
+}
+</style>
 
 <script>
 export default {
-  methods: {
-    setIsOpen: function() {
-      this.$store.state.isOpen = !this.$store.state.isOpen;
+    methods: {
+        setIsOpen: function() {
+            this.$store.state.isOpen = !this.$store.state.isOpen;
+        }
     }
-  }
 };
 </script>
